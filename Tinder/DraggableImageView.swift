cf44f1148abc535aed1d7a4a3b07112ee6c74629
Xiaofei Long <dreloong@gmail.com>
2016-04-15 18:02:54 -0500

    Enable dragging photo horizontally

--- Tinder/DraggableImageView.swift
+++ Tinder/DraggableImageView.swift
@@ -13,6 +13,8 @@ class DraggableImageView: UIView {
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var imageView: UIImageView!
 
+    var initialImageViewCenter: CGPoint!
+
     var image: UIImage? {
         get { return imageView.image }
         set { imageView.image = newValue }
@@ -37,5 +39,26 @@ class DraggableImageView: UIView {
         imageView.userInteractionEnabled = true
         imageView.clipsToBounds = true
         imageView.contentMode = .ScaleAspectFill
+    }
+
+    @IBAction func onPan(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .Began:
+            initialImageViewCenter = imageView.center
+        case .Changed:
+            let translation = sender.translationInView(self)
+            var offset = translation.x
+            if translation.x > 100 {
+                offset = 320
+            } else if translation.x < -100 {
+                offset = -320
+            }
+            imageView.transform = CGAffineTransformMakeTranslation(offset, 0)
+        case .Ended:
+            let offset = initialImageViewCenter.x - imageView.center.x
+            imageView.transform = CGAffineTransformMakeTranslation(offset, 0)
+        default:
+            break
+        }
     }
 }
